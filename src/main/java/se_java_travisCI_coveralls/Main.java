846c1fe38903ef5529e4d9abbc544bb5d1a1c1e8
Manuel Bothner <renhtob@gmail.com>
2016-05-06 12:01:12 +0200

    Creating a new Class Main.java - Put in a code failure to test Travis CI!

---
+++ src/main/java/se_java_travisCI_coveralls/Main.java
@@ -0,0 +1,15 @@
+package se_java_travisCI_coveralls;
+
+//### IMPORTS ##############################################################################################################################
+
+public class Main {
+
+	public static void main(String[] args) {
+	// TODO Auto-generated method stub
+		System.out.println("Hallo Welt");
+		System.
+	}
+	
+
+}
+//### EOF ##################################################################################################################################
