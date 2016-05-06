e4e949cdd3d4bd32663b4cd860125dcc2928104a
Manuel Bothner <renhtob@gmail.com>
2016-05-06 15:09:48 +0200

    New class Sum!

--- src/main/java/se_java_travisCI_coveralls/Sum.java
+++ src/main/java/se_java_travisCI_coveralls/Sum.java
@@ -1,20 +1,9 @@
 package se_java_travisCI_coveralls;
-
 //### IMPORTS ##############################################################################################################################
+public class Sum {
 
-public class Main {
-
-	public static void main(String[] args) {
-
-		//Calculate a sum of two integers:
-		int a = 10;
-		int b = 20;
-		
-		int sum = Sum.sumInt(a, b);
-	
-		System.out.println("The sum of a = " + a + " and b = " + b + " is " + sum);
+	public static int sumInt(int a, int b) {
+		return a + b;
 	}
-	
-
 }
 //### EOF ##################################################################################################################################
