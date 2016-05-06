08894f269b73e95f6cb2dc0d573552641422b137
Manuel Bothner <renhtob@gmail.com>
2016-05-06 15:41:04 +0200

    test private constructor

--- src/main/java/se_java_travisCI_coveralls/Sum.java
+++ src/main/java/se_java_travisCI_coveralls/Sum.java
@@ -1,7 +1,9 @@
 package se_java_travisCI_coveralls;
 //### IMPORTS ##############################################################################################################################
 public class Sum {
-
+    
+	private Sum() {}
+	
 	public static int sumInt(int a, int b) {
 		return a + b;
 	}
