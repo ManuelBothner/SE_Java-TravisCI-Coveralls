f5db137414c24bf174eeb8c6b4be3562802744f7
Manuel Bothner <renhtob@gmail.com>
2016-05-06 12:08:18 +0200

    Bugfix - To test Travis CI!

--- src/main/java/se_java_travisCI_coveralls/Main.java
+++ src/main/java/se_java_travisCI_coveralls/Main.java
@@ -7,7 +7,7 @@ public class Main {
 	public static void main(String[] args) {
 	// TODO Auto-generated method stub
 		System.out.println("Hallo Welt");
-		System.
+		System.out.println("Now the bug is fixed");
 	}
 	
 
