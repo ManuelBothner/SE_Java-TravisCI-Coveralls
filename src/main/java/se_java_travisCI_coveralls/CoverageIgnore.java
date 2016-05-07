3e8e98fb7100a6d4285e56c5f4bdec2dc8b43ec8
Manuel Bothner <renhtob@gmail.com>
2016-05-07 14:37:13 +0200

    Cobertura Ignore with annotation

--- src/main/java/se_java_travisCI_coveralls/CoverageIgnore.java
+++ src/main/java/se_java_travisCI_coveralls/CoverageIgnore.java
@@ -1,22 +1,7 @@
 package se_java_travisCI_coveralls;
-
-
 //### IMPORTS ##############################################################################################################################
-
-public class Main {
-	
-	@CoverageIgnore
-	public static void main(String[] args) {
-
-		//Calculate a sum of two integers:
-		int a = 10;		// $COVERAGE-IGNORE$
-		int b = 20;
-		
-		int sum = Sum.sumInt(a, b);
-	
-		System.out.println("The sum of a = " + a + " and b = " + b + " is " + sum);
-	}
-	
+public @interface CoverageIgnore {
 
 }
+
 //### EOF ##################################################################################################################################
